a27204287a2af3731de5671e34bd9c293c1da197
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2023-01-12 14:12:36 +0000

    Added clearInfo function at script.js

--- js/script.js
+++ js/script.js
@@ -40,3 +40,9 @@ function orderedFractions(limit) {
     fractions.sort();
     return fractionValues[fractions[fractions.length-1]][0];
 }
+
+// Function to Clear Information
+function clearInfo() {
+    let txt = "";
+    document.getElementById("numinfo").innerHTML = txt;
+}
